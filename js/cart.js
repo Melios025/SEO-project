49724aa9d50e0395e0c7b737ca5ab8f7e490a332
Melios025 <84947414+Melios025@users.noreply.github.com>
2021-12-05 07:28:38 +0700

    add checkout page, more responsive for cart

--- js/cart.js
+++ js/cart.js
@@ -3,3 +3,11 @@ $("#header").load("../assets/share/headernav.html");
 // load header when have id footer
 $("#footer").load("../assets/share/footer.html");
 
+$('#checkout').click(function (e) { 
+    e.preventDefault();
+    window.location.href = '../html/checkout.html'
+});
+$('#backShop').click(function (e) { 
+    e.preventDefault();
+    history.back();
+});
---
+++ js/checkout.js
@@ -0,0 +1,23 @@
+// load header when have id header
+$("#header").load("../assets/share/headernav.html");
+// load header when have id footer
+$("#footer").load("../assets/share/footer.html");
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
